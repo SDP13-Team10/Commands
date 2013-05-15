b255624d283f2f9e9608fbb1201bbb262d5fc0ab
Eric Moore <eric.o.moore@gmail.com>
2013-05-15 16:39:33 -0400

    Initial Commit

---
+++ commands.py
@@ -0,0 +1,49 @@
+modes =	("NORMAL",  "CHECK_ID",  "READ", "SET", "TEACHER")
+
+stuff =	{
+	'00'  : "NORMAL", \
+	'01'  : "CHECK_ID", \
+	'02'  : "READ",\
+	'03'  : "SET", \
+	'04'  : "TEACHER", \
+	'05'  : "GOOD", \
+	'06'  : "WRONG", \
+
+	'07'  : "WAKE_UP",\
+	'08'  : "GET_TIME", \
+	'09'  : "RESET", \
+	'10' : "SPEAK_TIME"
+	}
+MODES =	{
+	'0'  : "NORMAL", \
+	'1'  : "CHECK_ID", \
+	'2'  : "READ",\
+	'3'  : "SET", \
+	'4'  : "TEACHER"
+	}
+
+COMMAND = {
+	'0'  : "GOOD", \
+	'1'  : "WRONG", \
+	'2'  : "WAKE_UP",\
+	'3'  : "GET_TIME", \
+	'4'  : "RESET", \
+	'5'  : "SPEAK_TIME"
+	}
+	
+command = {
+	"good"		: '0', \
+	"wrong"		: '1', \
+	"wake_up"	: '2',\
+	"get_time"	: '3',\
+	"reset"		: '4',\
+	"speak_time"	: '5'
+}
+
+modeLookUp = {
+	"normal"	: '0',\
+	"check_id"	: '1',\
+	"read"		: '2',\
+	"set"		: '3',\
+	"teacher"	: '4'
+	}
