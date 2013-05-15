42254bf36e59c2d50540e60426baf22659d041dc
Eric Moore <eric.o.moore@gmail.com>
2013-05-15 17:19:18 -0400

    incremental changes
    added ADMIN

--- commands.py
+++ commands.py
@@ -1,25 +1,12 @@
-modes =	("NORMAL",  "AUTH",  "READ", "SET", "TEACHER")
+modes =	("NORMAL",  "AUTH",  "READ", "SET", "TEACHER", "ADMIN")
 
-stuff =	{
-	'00'  : "NORMAL", \
-	'01'  : "AUTH", \
-	'02'  : "READ",\
-	'03'  : "SET", \
-	'04'  : "TEACHER", \
-	'05'  : "GOOD", \
-	'06'  : "WRONG", \
-
-	'07'  : "WAKE_UP",\
-	'08'  : "GET_TIME", \
-	'09'  : "RESET", \
-	'10' : "SPEAK_TIME"
-	}
 MODES =	{
 	'0'  : "NORMAL", \
 	'1'  : "AUTH", \
 	'2'  : "READ",\
 	'3'  : "SET", \
-	'4'  : "TEACHER"
+	'4'  : "TEACHER", \
+	'5'  : "ADMIN"
 	}
 
 COMMAND = {
@@ -32,18 +19,21 @@ COMMAND = {
 	}
 	
 command = {
-	"good"		: '0', \
-	"wrong"		: '1', \
-	"wake_up"	: '2',\
-	"get_time"	: '3',\
-	"reset"		: '4',\
+	"good"			: '0', \
+	"wrong"			: '1', \
+	"wake_up"		: '2',\
+	"get_time"		: '3',\
+	"reset"			: '4',\
 	"speak_time"	: '5'
 }
 
 modeLookUp = {
 	"normal"	: '0',\
-	"auth"	: '1',\
+	"auth"		: '1',\
 	"read"		: '2',\
 	"set"		: '3',\
-	"teacher"	: '4'
-	}
+	"teacher"	: '4',\
+	"admin"		: '5'
+}
+
+
